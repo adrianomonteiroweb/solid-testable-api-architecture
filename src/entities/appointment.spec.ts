18f0e82be86ee70458d435c90b634c263f9c840c
adriano <adrianomonteiroweb@gmail.com>
2022-09-17 14:07:24 -0300

    feat/appointment test for endsAt with invalid date

--- src/entities/appointment.spec.ts
+++ src/entities/appointment.spec.ts
@@ -3,12 +3,32 @@ import { expect, test } from 'vitest';
 import { Appointment } from './Appointment';
 
 test('Create an appointment', () => {
+  const startsAt = new Date();
+  const endsAt = new Date();
+
+  endsAt.setDate(endsAt.getDate() + 1);
+
   const appointment = new Appointment({
     customer: 'Adriano Monteiro',
-    startsAt: new Date(),
-    endsAt: new Date(),
+    startsAt,
+    endsAt,
   });
 
   expect(appointment).toBeInstanceOf(Appointment);
   expect(appointment.customer).toEqual('Adriano Monteiro');
+});
+
+test('Cannot create an appointment with and date before start date.', () => {
+  const startsAt = new Date();
+  const endsAt = new Date();
+
+  endsAt.setDate(endsAt.getDate() - 1);
+
+  expect(() => {
+    return new Appointment({
+      customer: 'Adriano Monteiro',
+      startsAt,
+      endsAt,
+    });
+  }).toThrow();
 });
